b456d270c1651e173e75c825223d567077fd5c6f
coach-rachel-cn <rachel@csedge.org>
2025-06-27 16:29:51 +0000

    move from Glitch

---
+++ pet.js
@@ -0,0 +1,15 @@
+console.log("pet script running")
+
+// Grab the HTML elements you need and store in variables
+
+
+
+// Create a variable to keep track of the number of pets
+
+
+
+// Add conditional logic and functions to make the button(s) work
+
+
+
+// Create event listeners for the pet button
